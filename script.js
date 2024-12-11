367502e55ecd025b4ebe2964456b8c508ce98895
Tristan Ross <Tristan@Tristans-MacBook-Pro-2.local>
2024-12-11 12:20:25 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+//Create a string to display the vault message to the user
+//Create three separate variables, each containing equations using unqiue arithmetic operators whose results equal the three numbers of the vault combination: 10 - 40 - 39
+//Create a dialog box (alert) containing the string and three variables
+
+//Message to be displayed to user
+const message = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+
+//Calculation of the first number of the vault combination
+const num1 = 500/50;
+
+//Calculation of the second number of the vault combination
+const num2 = 39 + 1;
+
+//Calculation of the third number of the vault combination
+const num3 = 40-1;
+
+//Pop-up message to be displayed to the user containing the inital message and the three numbers of the comination
+alert(`${message} ${num1} - ${num2} - ${num3}`);
